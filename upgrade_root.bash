1eb0d36fdda66084afe13e01fc03626a988d70eb
Manfred Touron <m@42.am>
2014-11-26 15:11:40 +0100

    Made upgrade_root.bash parametered #4

--- upgrade_root.bash
+++ upgrade_root.bash
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-tar --strip=2 -C / -xzvf <(wget -qO - https://github.com/online-labs/ocs-scripts/archive/master.tar.gz)
+BRANCH=${BRANCH:-master}
+FLAVORS=${FLAVORS:-$@}
+
+apply_flavor() {
+    flavor="$1"
+    tar --strip=2 -C / -xzvf <(wget -qO - https://github.com/online-labs/ocs-scripts/archive/$BRANCH.tar.gz) ocs-scripts-$BRANCH/skeleton$flavor;
+}
+
+# Apply default skeleton
+apply_flavor ""
+
+# Appply flavors if any
+for flavor in $FLAVORS; do apply_flavor -$flavor; done
