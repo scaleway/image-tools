0c2bfb6caefe6d8769b8588f0d6efb902dbd8846
Manfred Touron <m@42.am>
2014-11-27 17:17:33 +0100

    Made dynamic upstart install script

--- skeleton-upstart/tmp/ocs-scripts-install-upstart.sh
+++ skeleton-upstart/tmp/ocs-scripts-install-upstart.sh
@@ -2,5 +2,23 @@
 
 set -xe
 
-cat /etc/lsb-release | grep -i trusty >/dev/null && ln -s /etc/init.d/disconnectnbd /etc/rc6.d/S89disconnectnbd
-cat /etc/lsb-release | grep -i utopic >/dev/null && update-rc.d disconnectnbd defaults
+lsb_dist="$(. /etc/lsb-release && echo "$DISTRIB_ID")"
+lsb_release="$(. /etc/lsb-release && echo "$DISTRIB_RELEASE")"
+
+case "$lsb_dist" in
+    Ubuntu)
+        case "$lsb_release" in
+            14.04)
+                ln -s /etc/init.d/disconnectnbd /etc/rc6.d/S89disconnectnbd
+                exit 0
+                ;;
+            14.10)
+                update-rc.d disconnectnbd defaults
+                exit 0
+                ;;
+        esac
+        ;;
+esac
+
+echo "Unsupported distribution $DISTRIB_ID ($DISTRIB_RELEASE)" >&2
+exit 1
