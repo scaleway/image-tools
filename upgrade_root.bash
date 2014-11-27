70d74a507e072bbb30c6a1f4d5a63b304e4ffd60
Manfred Touron <m@42.am>
2014-11-27 16:19:06 +0100

    Added install script support

--- upgrade_root.bash
+++ upgrade_root.bash
@@ -6,6 +6,9 @@ FLAVORS=${FLAVORS:-$@}
 apply_flavor() {
     flavor="$1"
     tar --strip=2 -C / -xzvf <(wget -qO - https://github.com/online-labs/ocs-scripts/archive/$BRANCH.tar.gz) ocs-scripts-$BRANCH/skeleton$flavor;
+    if [ -x /tmp/ocs-scripts-install$flavor.sh ]; then
+        /tmp/ocs-scripts-install$flavor.sh
+    fi
 }
 
 # Apply default skeleton
