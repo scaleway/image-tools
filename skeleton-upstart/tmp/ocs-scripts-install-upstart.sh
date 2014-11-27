516a84ac0d8d50e8321f32aeec5c74c2cf0c9d2d
Manfred Touron <m@42.am>
2014-11-27 17:07:39 +0100

    Fixed upstart install

--- skeleton-upstart/tmp/ocs-scripts-install-upstart.sh
+++ skeleton-upstart/tmp/ocs-scripts-install-upstart.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-update-rc.d disconnectnbd defaults
+
+cat /etc/lsb-release | grep -i trusty >/dev/null && ln -s /etc/init.d/disconnectnbd /etc/rc6.d/S89disconnectnbd
+cat /etc/lsb-release | grep -i utopic && update-rc.d disconnectnbd defaults
