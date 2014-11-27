13fbadeb002a59fd1ea14d8a1b352be57875a923
Manfred Touron <m@42.am>
2014-11-27 17:09:35 +0100

    improved logging

--- skeleton-upstart/tmp/ocs-scripts-install-upstart.sh
+++ skeleton-upstart/tmp/ocs-scripts-install-upstart.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -xe
 
 cat /etc/lsb-release | grep -i trusty >/dev/null && ln -s /etc/init.d/disconnectnbd /etc/rc6.d/S89disconnectnbd
-cat /etc/lsb-release | grep -i utopic && update-rc.d disconnectnbd defaults
+cat /etc/lsb-release | grep -i utopic >/dev/null && update-rc.d disconnectnbd defaults
