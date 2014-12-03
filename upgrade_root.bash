2d57070b6b2569219d831b58103b34e961d5374c
Manfred Touron <m@42.am>
2014-12-03 14:41:50 +0100

    Decorated bash variables

--- upgrade_root.bash
+++ upgrade_root.bash
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 BRANCH=${BRANCH:-master}
-FLAVORS=$(echo ${FLAVORS:-$@} | tr "," " ")
+FLAVORS=$(echo ${FLAVORS:-${@}} | tr "," " ")
 ROOTDIR=${ROOTDIR:-/}
 
 apply_flavor() {
-    flavor="$1"
-    tar --strip=2 -C ${ROOTDIR}/ -xzvf <(wget -qO - https://github.com/online-labs/ocs-scripts/archive/$BRANCH.tar.gz) ocs-scripts-$BRANCH/skeleton$flavor;
-    if [ -x /tmp/ocs-scripts-install$flavor.sh ]; then
-        /tmp/ocs-scripts-install$flavor.sh
+    flavor="${1}"
+    tar --strip=2 -C ${ROOTDIR}/ -xzvf <(wget -qO - https://github.com/online-labs/ocs-scripts/archive/${BRANCH}.tar.gz) ocs-scripts-${BRANCH}/skeleton${flavor};
+    if [ -x /tmp/ocs-scripts-install${flavor}.sh ]; then
+        /tmp/ocs-scripts-install${flavor}.sh
     fi
 }
 
@@ -16,4 +16,4 @@ apply_flavor() {
 apply_flavor ""
 
 # Appply flavors if any
-for flavor in $FLAVORS; do apply_flavor -$flavor; done
+for flavor in ${FLAVORS}; do apply_flavor -${flavor}; done
