cecc9d94844a91ee114738d300c38ee517c0572d
Manfred Touron <m@42.am>
2015-02-11 15:52:51 +0100

    Added testMetadata test (#9)

--- unit.bash
+++ unit.bash
@@ -6,10 +6,27 @@ testRootPasswd() {
     assertEquals "User root has a setted password" 0 $returnCode
 }
 
+
 testKernelModules() {
     test -d "/lib/modules/$(uname -r)/kernel"
     returnCode=$?
     assertEquals "Kernel modules not downloaded" 0 $returnCode
 }
+
+
+testMetadata() {
+    which oc-metadata >/dev/null
+    returnCode=$?
+    assertEquals "oc-metadata not found" 0 $returnCode
+
+    test -n "$(oc-metadata --cached)"
+    returnCode=$?
+    assertEquals "Cannot fetch metadata" 0 $returnCode
+
+    local private_ip=$(ip addr list eth0 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1)
+    local metadata_ip=$(oc-metadata --cached PRIVATE_IP)
+    assertEquals "PRIVATE_IP from metadata does not match eth0" $private_ip $metadata_ip
+}
+
 
 . shunit2
