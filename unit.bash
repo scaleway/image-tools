d6896118166722cfaf3f7a1272c6bff6b9013006
Manfred Touron <m@42.am>
2015-02-11 15:45:20 +0100

    Added testKernelModules test (#9)

--- unit.bash
+++ unit.bash
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 testRootPasswd() {
-	grep 'root:\*\?:' /etc/shadow >/dev/null
-	returnCode=$?
-	assertEquals "User root has a setted password" 0 $returnCode
+    grep 'root:\*\?:' /etc/shadow >/dev/null
+    returnCode=$?
+    assertEquals "User root has a setted password" 0 $returnCode
+}
+
+testKernelModules() {
+    test -d "/lib/modules/$(uname -r)/kernel"
+    returnCode=$?
+    assertEquals "Kernel modules not downloaded" 0 $returnCode
 }
 
 . shunit2
