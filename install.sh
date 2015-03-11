c7e7616e8604d920007f24ac1463e4bd322cbd5a
Manfred Touron <m@42.am>
2015-03-11 12:28:26 +0100

    Using cached tarball to prevent multiple downloads

--- install.sh
+++ install.sh
@@ -1,31 +1,54 @@
 #!/bin/bash
 
+# Variables
 BRANCH=${BRANCH:-master}
 FLAVORS=$(echo ${FLAVORS:-${@:-"common"}} | tr "," " ")
 ROOTDIR=${ROOTDIR:-/}
 DL=${DL:-$(hash curl 2>/dev/null && echo "curl" || echo "wget")}
+TMPFILE=/tmp/ocs-scripts-temporary.tar
 
 
+# Download with curl or wget
 dl() {
     if [ "$DL" = "wget" ]; then
-        wget -O - --no-check-certificate $@
+        wget -q -O - --no-check-certificate $@
     fi
     if [ "$DL" = "curl" ]; then
-        curl -Lk $@
+        curl -sLk $@
     fi
 }
 
 
+# Apply the flavor directory over the ROOTDIR
 apply_flavor() {
-    flavor="${1}"
-    tar --strip=2 -C ${ROOTDIR}/ -xzvf <(dl https://github.com/online-labs/ocs-scripts/archive/${BRANCH}.tar.gz) ocs-scripts-${BRANCH}/skeleton-${flavor};
+    local flavor="${1}"
+    echo "applying flavor '${flavor}'"
+    tar --strip=2 -C ${ROOTDIR}/ -xzf ${TMPFILE} ocs-scripts-${BRANCH}/skeleton-${flavor};
+
+    # Executes flavor's install script
     if [ -x /tmp/ocs-scripts-install-${flavor}.sh ]; then
         /tmp/ocs-scripts-install-${flavor}.sh
     fi
 }
 
 
-# Appply flavors if any
-for flavor in ${FLAVORS}; do
-    apply_flavor ${flavor}
-done
+# Cleaning
+clean() {
+    rm -rf ${TMPFILE}
+}
+
+
+main() {
+    # Download tarball
+    dl https://github.com/online-labs/ocs-scripts/archive/${BRANCH}.tar.gz > ${TMPFILE}
+
+    # Apply flavors
+    for flavor in ${FLAVORS}; do
+        apply_flavor ${flavor}
+    done
+
+    clean
+}
+
+
+main
