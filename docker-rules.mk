0842ea70d3b5575e2aabe6158c273da60dc5bc8e
Manfred Touron <m@42.am>
2015-04-01 22:59:39 +0000

    Added make image command

--- docker-rules.mk
+++ docker-rules.mk
@@ -16,6 +16,7 @@ VERSION ?=              latest
 VERSION_ALIASES ?=
 BUILD_OPTS ?=
 HOST_ARCH ?=		$(shell uname -m)
+IMAGE_VOLUME_SIZE ?=	50G
 
 
 # Phonies
@@ -56,6 +57,15 @@ info:
 	@echo "- Docker image      $(DOCKER_NAMESPACE)$(NAME):$(VERSION)"
 	@echo "- S3 URL            $(S3_URL)/$(NAME)-$(VERSION).tar"
 	@test -f $(BUILDDIR)rootfs.tar && echo "- Image size        $(shell stat -c %s $(BUILDDIR)rootfs.tar | numfmt --to=iec-i --suffix=B --format=\"%3f\")" || true
+
+
+image:
+	s3cmd ls $(S3_URL)/$(NAME)-$(VERSION).tar | grep -q '.tar' \
+		|| $(MAKE) publish_on_s3.tar
+	test -f /tmp/create-image-from-s3.sh \
+		|| wget -qO /tmp/create-image-from-s3.sh https://github.com/moul/onlinelabs-cli/raw/master/examples/create-image-from-s3.sh
+	chmod +x /tmp/create-image-from-s3.sh
+	VOLUME_SIZE=$(IMAGE_VOLUME_SIZE) /tmp/create-image-from-s3.sh $(S3_URL)/$(NAME)-$(VERSION).tar
 
 
 release: build
